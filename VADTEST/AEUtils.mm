aae8ed082bebfcf4d6ce721dbf7e25930aac9810
zhangyu <zhangyu@juwang.cn>
2017-10-31 12:59:41 +0800

    update vad

--- VADTEST/AEUtils.mm
+++ VADTEST/AEUtils.mm
@@ -9,6 +9,7 @@
 #import "AEUtils.h"
 #import "TheAmazingAudioEngine.h"
 #import "VoiceActiveCheck.h"
+#import "WebrtcNS.h"
 
 @interface AEUtils() {
     
@@ -22,8 +23,12 @@
 
 @implementation AEUtils {
     webrtc::VoiceActiveCheck *m_ivad;
+    webrtc::WebrtcNS *m_ns;
     
     int16_t *m_ibuffer;
+    
+    int16_t **ns_inputBuffer;
+    int16_t **ns_outputBuffer;
 }
 
 - (instancetype)init
@@ -38,8 +43,14 @@
     };
     
     m_ivad = new webrtc::VoiceActiveCheck(48000);
+    m_ns = new webrtc::WebrtcNS(16000);
     
-    m_ibuffer = (int16_t *)malloc(sizeof(int16_t) * 1024);
+    m_ibuffer = (int16_t *)malloc(sizeof(int16_t) * 256);
+    
+    ns_inputBuffer = (int16_t **)malloc(sizeof(int16_t) * 1);
+    ns_inputBuffer[0] = (int16_t *)malloc(sizeof(int16_t) * 160);
+    ns_outputBuffer = (int16_t **)malloc(sizeof(int16_t) * 1);
+    ns_outputBuffer[0] =(int16_t *)malloc(sizeof(int16_t) * 160);
     return self;
 }
 
@@ -55,8 +66,20 @@
     for (int idx = 0; idx < context->frames; idx ++) {
         m_ibuffer[idx] = tBuffer[idx] * 32768.0f;
     }
-    int valid = m_ivad->isActiveVoice(m_ibuffer);
-    NSLog(@"vad status:%d",valid);
+//    long long t1 = [[NSDate date] timeIntervalSince1970] * 1000.0;
+//    int valid = m_ivad->isActiveVoice(m_ibuffer);
+//    long long t2 = [[NSDate date] timeIntervalSince1970] * 1000.0;
+//    NSLog(@"vad status:%d,time = %lld",valid, t2 - t1);
+    int fr_length = 0;
+    int es_noise = 0;
+    
+    memcpy(ns_inputBuffer[0], m_ibuffer, 160 * sizeof(short));
+    m_ns->processAudio(ns_inputBuffer, ns_outputBuffer, 1);
+    int *rvalue = m_ns->noise_estimate(&fr_length, &es_noise);
+    NSLog(@"estimate = %d", es_noise);
+    if (rvalue) {
+        
+    }
 }
 
 - (void)start
@@ -69,10 +92,10 @@
     if (![[AVAudioSession sharedInstance].mode isEqualToString:AVAudioSessionModeDefault]) {
         [[AVAudioSession sharedInstance] setMode:AVAudioSessionModeDefault error:NULL];
     }
-    [[AVAudioSession sharedInstance] setPreferredSampleRate:48000 error:nil];
-    [self.output setSampleRate:48000];
+    [[AVAudioSession sharedInstance] setPreferredSampleRate:16000 error:nil];
+    [self.output setSampleRate:16000];
     
-    [[AVAudioSession sharedInstance] setPreferredIOBufferDuration:1024.0 / 48000 error:NULL];
+    [[AVAudioSession sharedInstance] setPreferredIOBufferDuration:256.0 / 16000 error:NULL];
     [[AVAudioSession sharedInstance] setActive:YES error:NULL];
     
     [self.output start:NULL];
